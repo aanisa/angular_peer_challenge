04f7de38823f4d5fd2b357bcc21c3c21a27bfdfb
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-04-04 16:59:46 -0500

    styling and functionality including delete button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log(data);
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('ListingsController', ['$scope', function($scope) {
+  $scope.listings = data;
+
+  $scope.removeListing = function(index) {
+    console.log(index);
+    $scope.listings.splice(index, 1);
+  };
+}]);
